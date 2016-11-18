805b0f32571deb6792cd57d5ab3edb5e579eacdc
Jon Lauters <jonathan.lauters@gmail.com>
2016-11-18 10:58:29 -0600

    cleaned up / seperated view / main app stuff

--- src/main.js
+++ src/main.js
@@ -3,41 +3,12 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 
-// Load template components
-const Home = require('./Home.vue');
-Vue.component('home', Home)
-
-const Signup = require('./Signup.vue');
-Vue.component('signup', Signup)
-
-const Login = require('./Login.vue');
-Vue.component('login', Login)
-
-const UploadData = require('./UploadData.vue');
-Vue.component('upload-data', UploadData)
-
-const UserPrefs = require('./UserPrefs.vue');
-Vue.component('user-prefs', UserPrefs)
-
-const UserProfile = require('./UserProfile.vue');
-Vue.component('user-profile', UserProfile)
-
-const UserActivityLog = require('./UserActivityLog.vue');
-Vue.component('user-activity', UserActivityLog)
-
+const routes = require('./routes.js')
 const App = require('./App.vue')
-const store = require('./store.js');
+const store = require('./store.js')
 
 const router = new VueRouter({
-  routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/signup', name: 'signup', component: Signup },
-    { path: '/login', name: 'login', component: Login },
-    { path: '/upload', name: 'upload-data', component: UploadData },
-    { path: '/user/:id/preferences', name: 'user-prefs', component: UserPrefs },
-    { path: '/user/:id/profile', name: 'user-profile', component: UserProfile },
-    { path: '/user/:id/activity', name: 'user-activity', component: UserActivityLog }
-  ]
+  routes: routes.default
 });
 
 new Vue({
@@ -45,7 +16,4 @@ new Vue({
   el: '#app',
   render: h => h(App),
   router: router,
-  ready: function() {
-    console.log('main app ready function');
-  }
 })
---
+++ src/routes.js
@@ -0,0 +1,35 @@
+// Vue Routes Config
+import Vue from 'vue'
+
+
+// Load Template Components
+const Home = require('./Home.vue')
+Vue.component('home', Home)
+
+const Signup = require('./Signup.vue')
+Vue.component('signup', Signup)
+
+const Login = require('./Login.vue')
+Vue.component('login', Login)
+
+const UploadData = require('./UploadData.vue')
+Vue.component('upload-data', UploadData)
+
+const UserPrefs = require('./UserPrefs.vue')
+Vue.component('user-prefs', UserPrefs)
+
+const UserProfile = require('./UserProfile.vue')
+Vue.component('user-profile', UserProfile)
+
+const UserActivityLog = require('./UserActivityLog.vue')
+Vue.component('user-activity', UserActivityLog)
+
+export default [
+  { path: '/', name: 'home', component: Home },
+  { path: '/signup', name: 'signup', component: Signup },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/upload', name: 'upload-data', component: UploadData },
+  { path: '/user/:id/preferences', name: 'user-prefs', component: UserPrefs },
+  { path: '/user/:id/profile', name: 'user-profile', component: UserProfile },
+  { path: '/user/:id/activity', name: 'user-activity', component: UserActivityLog }
+]
