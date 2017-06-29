1fa24383f291dc4fc53981fbc53b475caae86324
Maria Passarotti <maria@whirl-i-gig.com>
2017-06-29 05:42:55 -0400

    profile view

--- src/MyProfile.vue
+++ src/MyProfile.vue
@@ -1,100 +1,60 @@
 <template>
 <div id="user-profile">
-  <div class="row">
-    <div class="col-sm-12">
-      <div class="page-header">
-        <h1>Your Profile</h1>
-      </div>
+	<div class="row">
+    	<div class="col-sm-6">
+					<div class="card card-gray">
+						<div class="card-header text-center">
+							Your Profile
+						</div>
+						<div class="card-block">
+						
+				
+						<div>Name: {{user.name}}</div>
+						<div>Email: {{user.email}}</div>
+						<div>Location: {{user.location}}</div>
+						<div>Tagline: {{user.tagline}}</div>
+						<div>URL: {{user.url}}</div>
+						<div class="text-center">
+							<button v-on:submit.prevent="editPrefs" v-on:click.prevent="editPrefs" class="btn btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</button>
+						</div>
+						</div>
+			</div>
+		</div><!-- end col -->
+        <div class="col-sm-6">
+			
+			<div class="card card-gray">
+				<div class="card-header card-header-with-tabs">
+    				<ul class="nav nav-tabs card-header-tabs" role="tablist">
+						<li role="presentation" class="nav-item"><a href="#repos" aria-controls="repos" role="tab" data-toggle="tab" class="nav-link active">Repositories</a></li>
+						<!--<li role="presentation" class="nav-item"><a href="#orgs" aria-controls="orgs" role="tab" data-toggle="tab" class="nav-link">Organizations</a></li>-->
+						<li role="presentation" class="nav-item"><a href="#followers" aria-controls="followers" role="tab" data-toggle="tab" class="nav-link">Followers</a></li>
+						<li role="presentation" class="nav-item"><a href="#following" aria-controls="following" role="tab" data-toggle="tab" class="nav-link">Following</a></li>
+					</ul>
+				</div>
+			
+				<div class="tab-content">
+                	<div role="tabpanel" class="tab-pane active" id="repos">                  
+						<ul class="list-group list-group-flush" v-if="repos.length">
+							<li class="list-group-item justify-content-between" v-for="repo in repos">
+								<div><a @click="setActiveRepo(repo.id)">{{repo.name}}</a><br/>
+									<small>{{repo.url}} {{repo.created_on}}</small>
+								</div>
+								<div>
+									<router-link class="btn btn-primary btn-sm" :to="{path: '/edit-repo/' + repo.id, params: {id: repo.name}}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</router-link>
+									<click-confirm placement="top" style="display:inline;">
+										<a @click="deleteRepo(repo.id)" class="btn btn-orange  btn-sm"><i class="fa fa-times-circle" aria-hidden="true"></i> Delete</a>
+									</click-confirm>
+								</div>
+							</li>
+						</ul>
+					</div>
+                	<div role="tabpanel" class="tab-pane" id="orgs"> Orgs list to come ...</div>
+                	<div role="tabpanel" class="tab-pane" id="followers"> Followers list to come ... </div>
+                	<div role="tabpanel" class="tab-pane" id="following"> Following list to come ... </div>
+              	</div> 
+			</div>           
+        </div>
     </div>
-  </div>
-
-  <div class="row">
-    <div class="col-sm-12">
-      <div class="card">
-        <div class="card-block">
-        
-          <div class="row">
-            <div class="col-sm-6">  
-              <div class="card">
-                  <div class="card-block">
-                        <div class="pull-right">
-                            <button v-on:submit.prevent="editPrefs" v-on:click.prevent="editPrefs" class="btn btn-primary">Edit</button>
-                        </div>
-                
-                      <div>Name: {{user.name}}</div>
-                      <div>Email: {{user.email}}</div>
-                      <div>Location: {{user.location}}</div>
-                      <div>Tagline: {{user.tagline}}</div>
-                      <div>URL: {{user.url}}</div>
-
-                  </div>
-              </div>
-
-            </div> 
-          </div>
-
-
-          <!-- User Items / Relationships? -->
-          <div class="row">
-            <div class="col-sm-12">
-
-              <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="nav-item active"><a href="#repos" aria-controls="repos" role="tab" data-toggle="tab" class="nav-link">Repositories</a></li>
-                <!--<li role="presentation" class="nav-item"><a href="#orgs" aria-controls="orgs" role="tab" data-toggle="tab" class="nav-link">Organizations</a></li>-->
-                <li role="presentation" class="nav-item"><a href="#followers" aria-controls="followers" role="tab" data-toggle="tab" class="nav-link">Followers</a></li>
-                <li role="presentation" class="nav-item"><a href="#following" aria-controls="following" role="tab" data-toggle="tab" class="nav-link">Following</a></li>
-              </ul>
-
-              <div id="useritems" class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="repos">
- 
-                  <div class="card ">
-                    <div class="card-header">Repositories</div>
-                    <div class="card-block">
-                      <p></p> 
-                    </div>
-
-                    <table class="table table-hover">
-                      <thead>
-                      <tr>
-                        <th>Name</th>
-                        <th>URL</th>
-                        <th>Created On</th>
-                        <th>Controls</th>
-                      </tr>
-                      </thead>
-     
-                      <tbody>
-
-                        <tr v-for="repo in repos">
-                          <td>{{repo.name}}</td>
-                          <td>{{repo.url}}</td>
-                          <td>{{repo.created_on}}</td>
-                          <td>
-                            <router-link class="item" :to="{path: '/edit-repo/' + repo.id, params: {id: repo.name}}"><i class="fa fa-pencil" aria-hidden="true"></i></router-link> |
-                            <click-confirm placement="top" style="display: inline;">
-                                <a @click="deleteRepo(repo.id)"><i class="fa fa-trash" aria-hidden="true"></i></a>
-                            </click-confirm>
-                          </td>
-                        </tr> 
-                      </tbody>
-                    </table>
-
-                  </div>
-
-                </div>
-                <div role="tabpanel" class="tab-pane" id="orgs"> Orgs list to come ...</div>
-                <div role="tabpanel" class="tab-pane" id="followers"> Followers list to come ... </div>
-                <div role="tabpanel" class="tab-pane" id="following"> Following list to come ... </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div>  
-      </div>
-    </div>
-  </div>
 </div>
 
 </template>
