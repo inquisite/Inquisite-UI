9683b2c90c86b669a5a7812b9d343f8e0c410a8d
Seth <seth@collectiveaccess.org>
2017-09-25 21:43:17 -0400

    Update search to use ElasticSearch

--- components/Search.vue
+++ components/Search.vue
@@ -4,15 +4,22 @@
     <div class="col-sm-12">
       <div class="card card-form">
         <div class="card-header text-center">
-			Search results
+			Search results<span v-if="expression">: <em>{{expression}}</em></span>
 		 </div>
         <div class="card-block">
 
           <div id="search-msg" class="alert alert-danger" role="alert" v-show="message !== ''">{{message}}</div>
-            <h2>{{count}} found</h2>
-            <ol>
-                <li v-for="r, k in results"><router-link class="btn btn-primary btn-sm" :to="'/data/edit/' + r.data_id">Edit</router-link> <strong>From repository <u>{{r.repository_name}}</u>:</strong> {{r.display}}</li>
-            </ol>
+            <div v-if="results.length == 0"><h2>Nothing found</h2></div>
+            <div v-for="r,t in results">
+                <h2>{{t}} ({{counts[t]}})</h2>
+                <ol>
+                    <li v-if="t == 'Data'" v-for="v, k in r"><router-link class="btn btn-primary btn-sm" :to="'/data/edit/' + v.__id">Edit</router-link> <strong>From repository <em><u>{{v.__repo_name}}</u></em>:</strong> 
+                        {{displayDataForNode(v)}}
+                    </li>
+                    <li v-if="t == 'Repository'" v-for="v, k in r"><router-link class="btn btn-primary btn-sm" :to="'/data/edit/' + v.__id">View</router-link> {{v.name}}</li>
+                    <li v-if="t == 'Person'" v-for="v, k in r">{{v.forename}} {{v.surname}} ({{v.email}})</li>
+                </ol>
+            </div>
         </div>
       </div>
     </div>
@@ -35,12 +42,26 @@ export default {
 	results: function() {
 	    return this.$store.getters['search/getResults'];
 	},
+	counts: function() {
+	    return this.$store.getters['search/getCounts'];
+	},
 	count: function() {
-	    return this.results.length;
+	    return this.$store.getters['search/getCount'];
+	},
+	expression: function() {
+	    return this.$store.getters['search/getLastSearch'];
 	}
   },
   methods: {
-
+    displayDataForNode: function(v) {
+	    var disp = [];
+	    for(var i in v) {
+	        i = i + "";
+	        if (i.substr(0,2) == '__') { continue; }
+	        disp.push(i + "=" + v[i]);
+	    }
+	    return disp.join("; ");
+	}
   }   
 }
 </script>
--- store/modules/search.js
+++ store/modules/search.js
@@ -5,12 +5,16 @@ import { apiHeaders, extractAPIError } from '../../lib/utils.js'
 // initial state
 const state = {
   history: [],
-  results: []
+  results: {},
+  counts: {},
+  count: 0
 }
 
 // getters
 const getters = {
-     getResults: state => { return state.results; },
+     getResults: state => { return state.results; },// results per-type
+     getCounts: state => { return state.counts; },  // counts per-type
+     getCount: state => { return state.count; },    // total number of results across all types
      getHistory: state => { return state.history; },
      getLastSearch: state => { 
         return state.history[state.history.length - 1]; 
@@ -42,6 +46,8 @@ const mutations = {
         state.results = [];
         if (response.count > 0) {
             state.results = response.results;
+            state.counts = response.counts;
+            state.count = response.count;
             if (state.history.indexOf(response['expression']) === -1) { 
                 state.history.push(response['expression']); 
             }
