efe8582d42b6b4d280bde33981b8c98cb020be21
mwbenowitz <mwbenowitz@gmail.com>
2018-01-26 17:31:37 -0500

    Uploaded Statistics Basic View

--- components/UploadData.vue
+++ components/UploadData.vue
@@ -11,7 +11,7 @@
                     <flashmessage/>
 				</div>
 				<div class="card-block">
-          			<form id="signup-form" name="signup-form" method="POST" action="">            
+          			<form id="signup-form" name="signup-form" method="POST" action="">
 						<div class="form-group row">
 							<label for="name" class="col-2 col-form-label">File</label>
 							<div class="col-10">
@@ -28,9 +28,9 @@
 				<div class="card-block">
 					<div id="teaser-container" v-if="is_uploading">
 						<p><i class="fa fa-cog fa-spin fa-2x fa-fw" v-if="is_uploading"></i> Upload {{upload_progress}}% complete</p>
-						
+
 					</div>
-				</div>  
+				</div>
       		</div>
       	</div>
       	<div class="col-sm-12" v-if="(server_file_info !== null) && !import_complete">
@@ -42,31 +42,70 @@
       		        <div class="pull-right">
       		             <button v-on:click.prevent="setImportForAllFields" class="btn btn-sm">Create new fields for all unrecognized</button>
       		        </div>
-      		        
+
       		        <h3>Displaying first rows from <em>{{server_file_info.original_filename}}</em></h3>
       		        Import as <select name="import_info" v-model="import_as"><option v-for="(h,x) in data_types" :value="h.id">{{h.code}}</option></select>
-      	
-      		        
+
+
                     <div class="pull-right">
                             <input type="checkbox" v-model="ignore_rows"/>
                             Ignore first <input type="type" size="4" v-model="ignore_first_rows" v-if="ignore_rows"/> row(s)
                     </div>
-      		        
+
       		        <div class="item" style="padding: 10px 0">
                         <button v-on:click.prevent="importData" class="btn btn-primary" :disabled="!canImport">Import</button>
                         <a href="#" class="btn btn-secondary">Cancel</a>
                         <i class="fa fa-cog fa-spin fa-2x fa-fw" v-if="is_importing"></i>
                     </div>
 				</div>
+				<div class="card-block">
+					<h5>Basic Statistics</h5>
+					<p><strong>File Size</strong> {{filesizeInKB}}</p>
+					<p><strong>Total Columns</strong> {{server_file_info.total_columns}}</p>
+					<p><strong>Total Rows</strong> {{server_file_info.total_rows}}</p>
+				</div>
 				<div class="card-block" style="overflow: auto;">
+					<div v-for="(h,i) in server_file_info.preview.headers" class="row">
+						<div class="col-sm-2">
+							<h6>{{h}}</h6>
+							<button type="button" class="btn btn-primary" data-toggle="modal" :data-target="'#' + h + 'Stats'">Stats</button>
+
+							<div class="modal fade" :id="h + 'Stats'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							  <div class="modal-dialog" role="document">
+							    <div class="modal-content">
+							      <div class="modal-header">
+							        <h5 class="modal-title" id="exampleModalLabel">Stats</h5>
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							          <span aria-hidden="true">&times;</span>
+							        </button>
+							      </div>
+							      <div class="modal-body">
+									  <ul>
+  										<li v-for="(t, s) in server_file_info.column_stats[h]"><strong>{{s}}: </strong>{{t}}</li>
+  									</ul>
+							      </div>
+							      <div class="modal-footer">
+							        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							      </div>
+							    </div>
+							  </div>
+							</div>
+						</div>
+						<div class="col-sm-3 columnSelect">
+							<select :name="'import_info_' + h" v-model="data_mapping[i]"><option v-for="(h,x) in mappingOptions['options'][i]" :value="mappingOptions['values'][i][x]">{{h}}</option></select>
+						</div>
+						<div class="col-sm-3 columnSelect">
+							<select :name="'field_type_' + h" v-model="type_mapping[i]" >
+								<option v-for="(t,k) in fieldDataTypes" :value="k">{{t.name}}</option>
+							</select>
+						</div>
+						<div class="col-sm-4">
+
+						</div>
+					</div>
       		        <table>
       		            <tr>
                             <th v-for="(h,i) in server_file_info.preview.headers">{{h}}</th>
-      		            </tr>
-      		            <tr>
-                            <th v-for="(h,i) in server_file_info.preview.headers">
-                                <select :name="'import_info_' + h" v-model="data_mapping[i]"><option v-for="(h,x) in mappingOptions['options'][i]" :value="mappingOptions['values'][i][x]">{{h}}</option></select>
-                            </th>
       		            </tr>
       		            <tr v-for="(r, c) in server_file_info.preview.data">
       		                <td v-for="k,h in server_file_info.preview.headers" v-if="((!ignore_rows && (c >= 0)) || (ignore_rows && (c > ignore_first_rows)))">
@@ -77,7 +116,7 @@
       		    </div>
       		</div>
     	</div>
-    	
+
     	<div class="col-sm-12" v-if="import_complete">
     	    <div class="card card-form">
       		    <div class="card-header text-center">
@@ -85,7 +124,7 @@
 		 		</div>
 				<div class="card-block">
       		        <h3>Import from <em>{{server_file_info.original_filename}}</em> completed</h3>
-      		        
+
       		        <div class="item" style="padding: 10px 0">
                       <button v-on:submit.prevent="reset" v-on:click.prevent="reset" class="btn btn-primary">Do another import</button>
                     </div>
@@ -95,14 +134,14 @@
       		        <div v-for="(errs, line) in importErrors">
       		            Line {{line}}: {{errs}}
       		        </div>
-      		        
+
       		        <div v-if="import_results.counts.length > 0">
                         <h4>Counts</h4>
                         <div v-for="(c, t) in import_results.counts">
                             {{t}}: {{c}} items
                         </div>
                     </div>
-      		        
+
       		    </div>
       		</div>
     	</div>
@@ -113,7 +152,7 @@
 
 <script>
 
-export default { 
+export default {
   name: 'upload-data',
   data: function() {
     return {
@@ -124,15 +163,17 @@ export default {
       is_importing: false,
       import_complete: false,
       upload_progress: 0,
-      
+
       server_file_info: null,
-      
+
       data_mapping: [],
       import_as: null,
       ignore_rows: false,
       ignore_first_rows: 0,
-      
-      import_results: null
+
+      import_results: null,
+
+	  type_mapping: []
     }
   },
   mounted: function(){
@@ -141,38 +182,42 @@ export default {
   },
   computed: {
     isLoggedIn: function() { return this.$store.getters.isLoggedIn; },
-    data_types: function() { 
-        var t = this.$store.getters['schema/getDataTypes']; 
-        console.log("xxx", t);
-        if((t.constructor !== Array) || (t.length == 0)){ 
+    data_types: function() {
+        var t = this.$store.getters['schema/getDataTypes'];
+		console.log("xxx", t);
+        if((t.constructor !== Array) || (t.length == 0)){
             t =[{'id': -1, 'code': "< Create new type >"}];
         }
         return t;
     },
-    field_data_types: function() { this.$store.getters['schema/getFieldDataTypeList']; },
+    fieldDataTypes: function() { return this.$store.getters['schema/getFieldDataTypeList']; },
 	repos: function() { return this.$store.getters['people/getUserRepos']; },
 	user: function() { return this.$store.getters['people/getUserInfo']; },
 	activeRepo: function() { return this.$store.getters['repos/getActiveRepo']; },
-	
+
 	canImport: function() {
 	    return this.data_mapping.filter(function(v) {
 	        return v != 0;
 	    }).length > 0;
 	},
-	
+	filesizeInKB: function(){
+		var bytes = this.server_file_info.filesize;
+		var kb = (bytes/1000).toFixed(2);
+		return kb + ' kB';
+	},
 	mappingOptions: function() {
 	    var opts = [];
 	    var vals = [];
-	    
+
 	    console.log("import as ", this.import_as);
-	    var data_types = this.data_types;
+		var data_types = this.data_types;
 	    for(var i in this.server_file_info.preview.headers) {
             if (!opts[i]) { opts[i] = []; vals[i] = [] }
-            
+
             var allowCreateNew = true;
             for(var j in data_types) {
                 var dt = data_types[j];
-                
+
                 var match_id = null;
                 if (parseInt(this.import_as) < 0) {
                     opts[i].unshift("Create field " + this.server_file_info.preview.headers[i]);
@@ -185,14 +230,14 @@ export default {
                             match_id = dt['fields'][k]['id'];
                             //console.log("match", dt['fields'][k]['code'], match_id);
                         }
-                        
+
                         var f = this.data_mapping.indexOf(dt['fields'][k]['id']);
                         if ((f === -1) || (parseInt(f) === parseInt(i))) {
                             opts[i].unshift(dt['fields'][k]['name'] + " (" + data_types[j]['name'] + ")");
                             vals[i].unshift(dt['fields'][k]['id']);
                         }
                     }
-            
+
                     if (allowCreateNew) {
                         opts[i].unshift("Create field " + this.server_file_info.preview.headers[i] + " (" + data_types[j]['name'] + ")");
                         vals[i].unshift(this.server_file_info.preview.headers[i].replace(/[^A-Za-z0-9_\-]+/, ""));
@@ -209,9 +254,9 @@ export default {
                             vals[i].push(dt['fields'][k]['id']);
                         }
                     }
-                }   
+                }
             }
-            
+
             opts[i].unshift("Do not import");
             vals[i].unshift(0);
 	    }
@@ -220,10 +265,10 @@ export default {
 	},
 	importErrors: function() {
 	    if (!this.import_complete) { return []; }
-	    
+
 	    return _.mapValues(this.import_results.errors, function(v, k, o) { return v.join("; ") });
 	}
-  }, 
+  },
   methods: {
     onFileChange: function(e) {
       var files = e.target.files || e.dataTransfer.files;
@@ -237,12 +282,11 @@ export default {
         var self = this;
         if(this.data_file) {
             this.is_uploading = true;
-            this.$store.dispatch('data/uploadData', {form: { data_file: this.data_file, repo_id: this.activeRepo.id }, progressCallback: function(p) {
+			this.$store.dispatch('data/uploadData', {form: { data_file: this.data_file, repo_id: this.activeRepo.id }, progressCallback: function(p) {
                 self.upload_progress = p;
             }}).then(function(data) {
                 if (data['filename']) { self.server_file_info = data; }
                  self.is_uploading = false;
-                 
                 self.import_as = self.data_types[0]['id'];
                 self.data_mapping = Array(self.server_file_info.preview.headers.length).fill(0);
             });
@@ -251,7 +295,7 @@ export default {
     importData: function() {
         var self = this;
         this.is_importing = true;
-        this.$store.dispatch('data/importData', {data: { repo_id: this.activeRepo.id, filename: this.server_file_info.filename, data_mapping: this.data_mapping.join("|"), type: this.import_as, ignore_first: this.ignore_first_rows, original_filename: this.server_file_info.original_filename }}).then(function(data) { 
+        this.$store.dispatch('data/importData', {data: { repo_id: this.activeRepo.id, filename: this.server_file_info.filename, data_mapping: this.data_mapping.join("|"), type: this.import_as, ignore_first: this.ignore_first_rows, original_filename: this.server_file_info.original_filename }}).then(function(data) {
             self.import_results = {
                 "errors": data.errors,
                 "error_count": data.error_count,
@@ -260,7 +304,7 @@ export default {
             };
             self.is_importing = false;
             self.import_complete = true;
-            
+
             self.$store.dispatch('people/getRepos', {});    // reload stats... TODO: break repo stats out into a single repo-specific call
         });
     },
@@ -278,7 +322,7 @@ export default {
         }
         this.mappingOptions;
     }
-  } 
-  
+  }
+
 }
 </script>
