0584c6b44372430d280d96b5464c03e7651bf36b
mwbenowitz <mwbenowitz@gmail.com>
2018-03-23 12:40:12 -0400

    Search paging and improved UI

--- components/Search.vue
+++ components/Search.vue
@@ -12,18 +12,36 @@
           <flashmessage/>
             <div v-if="results.length == 0"><h2>Nothing found</h2></div>
             <div v-else>
-                <tabs>
-                    <tab v-if="counts[t] > 0" v-for="r,t in results" :name="t + ' (' + counts[t] + ' of ' + totalCounts[t] + ')'">
+                <tabs :options="{useUrlFragment: false}">
+                    <tab v-if="counts[t] > 0" v-for="r,t in results" :name="t + ' (' + totalCounts[t] + ')'">
+                        <div class="row paging-buttons" v-if="totalCounts[t] > 12">
+                            <div class="col-12 col-sm-6 offset-sm-3 text-center">
+                                <button v-on:click.prevent="loadPage(expression, t, slices[t][0]-13, (slices[t][0]-1))" class="btn btn-primary btn-orange" :disabled="(slices[t][0]-12 < 0)">Prev</button>
+                                <h6 class="paging-counts">{{slices[t][0]}} - {{slices[t][1]}}</h6>
+                                <button v-on:click.prevent="loadPage(expression, t, slices[t][1], (slices[t][1]+12))" class="btn btn-primary btn-orange" :disabled="(slices[t][1]+12 > totalCounts[t])">Next</button>
+                            </div>
+                        </div>
                         <div class="row">
                             <div v-for="v, k in r" class="col-12 col-sm-12 col-md-6 col-lg-3">
                                 <div class="card">
                                     <div class="card-block search-result-block">
                                         <div class="row">
-                                            <div class="col-5 text-left search-result-text">
+                                            <div v-if="t != 'Person'" class="col-5 text-left search-result-text">
                                                 <strong>Repository</strong><br/><em><u>{{v.__repo_name}}</u></em><br/>
-                                                <strong>Schema</strong><br/><em><u>{{v.__schema_name}}</u></em>
+                                                <div v-if="t != 'Repository'" class="search-result-text">
+                                                    <strong>Schema</strong><br/><em><u>{{v.__schema_name}}</u></em>
+                                                </div>
                                             </div>
-                                            <div class="col-6 text-right search-result-text">
+                                            <div v-else class="col-12 text-left search-result-text">
+                                                <strong>Repositories</strong>
+                                                <div v-for="z in v.__repos" class="search-result-text">
+                                                    {{z}}
+                                                </div>
+                                                <div v-if="!('__repos' in v)" class="search-result-text">
+                                                    None
+                                                </div>
+                                            </div>
+                                            <div v-if="t != 'Person'" class="col-6 text-right search-result-text">
                                                 <router-link v-if="t == 'Data'" class="btn btn-primary btn-block" :to="'/data/edit/' + v.__id">Edit</router-link>
                                                 <router-link v-if="(t == 'SchemaField' || t == 'SchemaType') && v.__repo_id == activeRepoID" class="btn btn-primary btn-block" :to="'/schema/edit/' + v['__schema_id']">Edit</router-link>
                                                 <button v-if="v.__repo_id != activeRepoID && t != 'Data'" class="btn btn-secondary" disabled><small>Load Repo</small></button>
@@ -66,12 +84,12 @@ export default {
   name: 'search',
   data: function() {
     return {
-      state: this.$store.state
+      state: this.$store.state,
+      initialTab: false
     }
   },
   computed: {
 	results: function() {
-        console.log("hello", this.$store.getters['search/getResults']);
 	    return this.$store.getters['search/getResults'];
 	},
 	counts: function() {
@@ -79,6 +97,9 @@ export default {
 	},
     totalCounts: function() {
         return this.$store.getters['search/getTotalCounts'];
+    },
+    slices: function() {
+        return this.$store.getters['search/getSlices'];
     },
 	count: function() {
 	    return this.$store.getters['search/getCount'];
@@ -89,6 +110,25 @@ export default {
     activeRepoID: function() {
         return this.$store.getters['repos/getActiveRepoID'];
     }
+  },
+  updated: function(){
+    var clicked = false;
+    if(!this.initialTab){
+        var tab_els = document.querySelectorAll("a[role='tab']");
+        console.log(document, tab_els);
+        for(var i = 0; i < tab_els.legnth; i++){
+            if(tab_els[i].href.indexOf('Data') > -1){
+                var tab_el = tab_els[i];
+                tab_el.click();
+                clicked = true;
+                break;
+            }
+        }
+        if(!clicked){
+            tab_els[0].click();
+        }
+    }
+    this.initialTab = true;
   },
   methods: {
     displayDataForNode: function(v) {
@@ -102,7 +142,10 @@ export default {
             disp[i] = v[i];
 	    }
 	    return disp;
-	}
+	},
+    loadPage: function(exp, node, start, end) {
+        this.$store.dispatch('search/pagingSearch', [exp, node, start, end]);
+    }
   }
 }
 </script>
--- store/modules/search.js
+++ store/modules/search.js
@@ -8,7 +8,8 @@ const state = {
   results: {},
   counts: {},
   count: 0,
-  total_counts: {}
+  total_counts: {},
+  slices: {}
 }
 
 // getters
@@ -17,6 +18,7 @@ const getters = {
      getCounts: state => { return state.counts; },  // counts per-type
      getCount: state => { return state.count; },    // total number of results across all types
      getTotalCounts: state => { return state.total_counts; }, // total number of results found (not just this page)
+     getSlices: state => { return state.slices; },
      getHistory: state => { return state.history; },
      getLastSearch: state => {
         return state.history[state.history.length - 1];
@@ -33,8 +35,22 @@ const actions = {
 
         return api.get('/search?q=' + escape(q), {headers: apiHeaders({"auth": false, "form": true})})
             .then(function(response) {
-                console.log("QUICK", response);
                 context.commit('QUICK_SEARCH', response);
+                return response;
+            }).catch(function(error) {
+                context.commit('API_FAILURE', error, {'root': true });
+                return extractAPIError(error);
+            })
+    },
+    pagingSearch: function(context, qArray){
+        if(!context.rootState.token) return false;
+        var q = qArray[0];
+        var node = qArray[1];
+        var start = qArray[2];
+        var end = qArray[3];
+        return api.get('/pagingSearch?q=' + escape(q) + '&n=' + escape(node) + '&s=' + escape(start) + '&e=' + escape(end), {headers: apiHeaders({"auth": false, "form": true})})
+            .then(function(response) {
+                context.commit('PAGING_SEARCH', [response, node]);
                 return response;
             }).catch(function(error) {
                 context.commit('API_FAILURE', error, {'root': true });
@@ -51,10 +67,20 @@ const mutations = {
             state.results = response.results;
             state.counts = response.counts;
             state.count = response.count;
+            state.slices = response.slices;
             state.total_counts = response.total_counts;
             if (state.history.indexOf(response['expression']) === -1) {
                 state.history.push(response['expression']);
             }
+        }
+    },
+    PAGING_SEARCH: function(state, rGroup) {
+        var response = rGroup[0];
+        var node = rGroup[1];
+        if (response.count > 0){
+            state.results[node] = response.results[node];
+            state.counts[node] = response.counts[node];
+            state.slices[node] = response.slices[node];
         }
     }
 }
