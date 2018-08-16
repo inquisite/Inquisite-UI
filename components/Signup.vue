30e63bc06457c4e158b26e26294828ffb9cc2ef2
mwbenowitz <mwbenowitz@gmail.com>
2018-08-16 14:02:45 -0400

    INQ-84 Fixed Login/Signup Forms
    The Bootstrapv4.1 handles forms different and this fixes those issues.
    Also streamlined the design of these screens somewhat by removing
    labels and adding reminders based on the form input that currently
    has focus

--- components/Signup.vue
+++ components/Signup.vue
@@ -19,63 +19,80 @@
               <div class="msg"></div>
             </div>
 
-			<div class="form-group row">
-				<label for="name" class="col-2 col-form-label">Name</label>
-				<div class="col-5">
-					<input type="text" class="form-control" id="forename" name="forename" placeholder="First name" v-model="forename">
-				</div>
-				<div class="col-5">
-					<input type="text" class="form-control" id="surname" name="surname" placeholder="Last name" v-model="surname">
-				</div>
-			</div>
-			
-			<div class="form-group row">
-				<label for="email" class="col-2 col-form-label">Email</label>
-				<div class="col-10">
-					<input type="text" class="form-control" id="email" name="email" placeholder="Email" v-model="email">
-				</div>
-			</div>
-			
-			<div class="form-group row" v-if="email.toLowerCase().indexOf('nyu') >= 0">
-				<label for="email" class="col-2 col-form-label">NetID</label>
-				<div class="col-10">
-					<input type="text" class="form-control" id="nyunetid" name="email" placeholder="NYU NetID" v-model="nyunetid">
-				</div>
-			</div>
-			
-			<div class="form-group row">
-				<label for="location" class="col-2 col-form-label">Location</label>
-				<div class="col-10">
-					<input type="text" class="form-control" id="location" name="location" placeholder="Location" v-model="location">
-				</div>
-			</div>
-            
-            <div class="form-group row">
-				<label for="tagline" class="col-2 col-form-label">Tagline</label>
-				<div class="col-10">
-					<input type="text" class="form-control" id="tagline" name="tagline" placeholder="Tagline" v-model="tagline">
-				</div>
+			<div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-6">
+                        <small id="fname-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'fname'">First name</small>
+    					<input type="text" class="form-control" id="forename" name="forename" placeholder="First name" v-model="forename" v-on:focus="setActive('fname')" v-on:blur="removeActive('fname')">
+    				</div>
+    				<div class="col-6">
+                        <small id="lname-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'lname'">Last name</small>
+    					<input type="text" class="form-control" id="surname" name="surname" placeholder="Last name" v-model="surname" v-on:focus="setActive('lname')" v-on:blur="removeActive('lname')">
+    				</div>
+                </div>
 			</div>
 
-            <div class="form-group row">
-				<label for="url" class="col-2 col-form-label">URL</label>
-				<div class="col-10">
-					<input type="text" class="form-control" id="url" name="url" placeholder="URL" v-model="url">
-				</div>
+			<div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="email-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'email'">Email</small>
+				        <input type="text" class="form-control" id="email" name="email" placeholder="Email" v-model="email" v-on:focus="setActive('email')" v-on:blur="removeActive('email')">
+                    </div>
+                </div>
 			</div>
-			
-			<div class="form-group row">
-				<label for="password" class="col-2 col-form-label">Password</label>
-				<div class="col-10">
-					<input type="password" class="form-control" id="password" name="password" placeholder="Password" v-model="password">
-				</div>
+
+			<div class="form-group mb-4" v-if="email.toLowerCase().indexOf('nyu') >= 0">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="netid-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'netid'">NYU ID</small>
+				        <input type="text" class="form-control" id="nyunetid" name="email" placeholder="NYU NetID" v-model="nyunetid" v-on:focus="setActive('netid')" v-on:blur="removeActive('netid')">
+                    </div>
+                </div>
 			</div>
-			
-			<div class="form-group row">
-				<label for="password" class="col-2 col-form-label">Confirm password</label>
-				<div class="col-10">
-					<input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirm password" v-model="password_confirm">
-				</div>
+
+			<div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="loc-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'loc'">Location</small>
+                        <input type="text" class="form-control" id="location" name="location" placeholder="Location" v-model="location" v-on:focus="setActive('loc')" v-on:blur="removeActive('loc')">
+                    </div>
+                </div>
+			</div>
+
+            <div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="tag-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'tag'">Tagline</small>
+                        <input type="text" class="form-control" id="tagline" name="tagline" placeholder="Tagline" v-model="tagline" v-on:focus="setActive('tag')" v-on:blur="removeActive('tag')">
+                    </div>
+                </div>
+			</div>
+
+            <div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="url-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'url'">URL</small>
+                        <input type="text" class="form-control" id="url" name="url" placeholder="URL" v-model="url" v-on:focus="setActive('url')" v-on:blur="removeActive('url')">
+                    </div>
+                </div>
+			</div>
+
+			<div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="pass-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'pass'">Password</small>
+                	    <input type="password" class="form-control" id="password" name="password" placeholder="Password" v-model="password" v-on:focus="setActive('pass')" v-on:blur="removeActive('pass')">
+                    </div>
+                </div>
+			</div>
+
+			<div class="form-group mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <small id="conf-help" class="form-text text-muted field-name-hint" v-if="this.activeField == 'conf'">Confirm Password</small>
+				        <input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirm password" v-model="password_confirm" v-on:focus="setActive('conf')" v-on:blur="removeActive('conf')">
+                    </div>
+                </div>
 			</div>
 
             <div class="form-group text-center">
@@ -96,7 +113,7 @@
 
 <script>
 import { validateEmail } from '../lib/utils.js'
-export default { 
+export default {
   name: 'home',
   data: function() {
     return {
@@ -109,42 +126,49 @@ export default {
       password: '',
       nyunetid: '',
       password_confirm: '',
-      
+      activeField: null,
+
       state: this.$store.state
     }
   },
   computed: {
-    
+
   },
   methods: {
     processSignup: function() {
       var self = this;
       if ((this.password.length > 0) && (this.password !== this.password_confirm)) {
-        this.$store.dispatch('setMessage', "Password do not match"); 
+        this.$store.dispatch('setMessage', "Password do not match");
       } else if( (this.forename === '') || (this.surname === '') || (this.email === '') || (this.password === '')) {
-        this.$store.dispatch('setMessage', "Name, Email, and Password are required fields"); 
+        this.$store.dispatch('setMessage', "Name, Email, and Password are required fields");
       } else if(!validateEmail(this.email)) {
-        this.$store.dispatch('setMessage', "Email address is not valid"); 
+        this.$store.dispatch('setMessage', "Email address is not valid");
       } else {
-        this.$store.dispatch('register', 
+        this.$store.dispatch('register',
           { data: {
-            forename: this.forename, 
+            forename: this.forename,
             surname: this.surname,
-            email: this.email, 
-            location: this.location, 
-            tagline: this.tagline, 
+            email: this.email,
+            location: this.location,
+            tagline: this.tagline,
             nyunetid: this.nyunetid,
-            url: this.url, 
+            url: this.url,
             password: this.password
         }}).then(function(result) {
             if (result) {
                 self.$store.dispatch('setMessage', "You are now signed up!");
-                self.$router.push('/'); 
+                self.$router.push('/');
             }
         });
-      } 
+      }
+    },
+    setActive: function(name){
+        this.activeField = name;
+    },
+    removeActive: function(name){
+        this.activeField = null;
     }
-  } 
-  
+  }
+
 }
 </script>
